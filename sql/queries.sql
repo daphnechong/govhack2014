63b93a87eaf52b743b3b047cec29ad5f6e794112
Daphne Chong <daphne.h.chong@gmail.com>
2014-07-13 12:35:13 +1000

    add queries for extracting the start journey times

--- sql/queries.sql
+++ sql/queries.sql
@@ -22,3 +22,61 @@ create table trips (route_id  varchar(255),service_id varchar(255),trip_id varch
 alter table trips add primary key (trip_id);
 load data infile 'trips.txt' into table trips fields terminated by ',' enclosed by '"' lines terminated by '\n' ignore 1 lines;
 
+create table calendar (service_id varchar(255),monday int, tuesday int, wednesday int, thursday int, friday int, saturday int, sunday int, start_date datetime, end_date datetime);
+alter table calendar add index (service_id);
+alter table calendar add index (start_date);
+alter table calendar add index (end_date);
+load data infile 'calendar.txt' into table calendar fields terminated by ',' enclosed by '"' lines terminated by '\n' ignore 1 lines;
+
+
+create table calendar_dates (service_id varchar(255), date datetime, exception_type varchar(10));
+alter table calendar_dates add index (service_id);
+load data infile 'calendar_dates.txt' into table calendar_dates fields terminated by ',' enclosed by '"' lines terminated by '\n' ignore 1 lines;
+
+
+create table all_trip_duration (route_id varchar(255), trip_id varchar(255), route_long_name varchar(255), departure_time time, stop_id varchar(255), stop_name varchar(255), stop_sequence int);
+alter table all_trip_duration add index(trip_id);
+alter table all_trip_duration add index (stop_id);
+
+
+insert into all_trip_duration
+select r.route_id, t.trip_id, r.route_long_name, st.departure_time, s.stop_id, s.stop_name, st.stop_sequence 
+from trips t
+inner join routes r on r.route_id = t.route_id 
+inner join stop_times st on st.trip_id = t.trip_id
+inner join stops s on st.stop_id = s.stop_id
+order by t.trip_id desc
+
+
+
+create table all_aggregate_trip (trip_id varchar(255), origin_stop_id varchar(255), 
+origin_stop_name varchar(255), departure_time time, destination_stop_id varchar(255), destination_stop_name varchar(255), arrival_time time);
+alter table all_aggregate_trip add index(origin_stop_id);
+alter table all_aggregate_trip add index (destination_stop_id);
+
+insert into all_aggregate_trip
+select start_st.trip_id, start_st.stop_id, start_st.stop_name,
+start_st.departure_time, 
+td.stop_id, td.stop_name, td.departure_time
+from all_trip_duration td
+inner join all_trip_duration start_st on td.trip_id = start_st.trip_id
+where start_st.stop_id != td.stop_id
+and start_st.stop_sequence = 1
+and td.stop_sequence = (select max(stop_sequence) 
+from all_trip_duration atd
+where atd.trip_id = td.trip_id)
+
+
+select * from routes where agency_id in ('112', '116', '117', '11954', '125', '14', '22', '300', '301', '306', '307','92','CR','LR')
+
+
+select r.route_id, at.departure_time, 
+time_format(abs(timediff(at.departure_time, at.arrival_time)), "%T") as duration
+from calendar c
+inner join trips t on c.service_id = t.service_id
+inner join all_aggregate_trip at on at.trip_id = t.trip_id
+inner join routes r on r.route_id = t.route_id
+and c.monday = 1
+where start_date < '2014-07-15 00:00:00' and end_date > '2014-07-14 00:00:00'
+and at.departure_time < '24:00:00'
+order by at.departure_time
